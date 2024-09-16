e52c0050e35daa8415db53aa60eab4d082bf2692
Kirils Aleksejevs <aleksejevs2003@gmail.com>
2024-09-16 22:15:10 +0200

    thank you page zuende: style muss noch gemacht werden

--- index.js
+++ index.js
@@ -19,17 +19,23 @@ document.getElementById("bringenOderVersenden").addEventListener("change", funct
 //Die ersten 2 PLZ Zahlen vergleichen
 function findLocation() {
     var eingegebenePLZ = document.getElementById("plz_bringen").value.substring(0, 2);
+    var list = [];
 
     // PLZ prüfen und entsprechende Geschäftsstelle anzeigen
     if (eingegebenePLZ === "76") {
         document.getElementById("standorte").innerHTML = '<div class="location_badenbaden"> <p>76530 Baden-Baden<br>Sophienstraße 105</p> </div>';
+        list.push(["76530","Baden-Baden", "Sopienstraße 105"]);
+
     } else if (eingegebenePLZ === "60") {
         document.getElementById("standorte").innerHTML = '<div id="location_frankfurt"> <p class="plz">60318 Frankfurt<br>Bachstraße 15</p> </div>';
+        list.push(["60318","Frankfurt", "Bachstraße 15"]);
     } else if (eingegebenePLZ === "70") {
         document.getElementById("standorte").innerHTML = '<div id="location_stuttgart"> <p class="plz">70176 Stuttgart<br>Königstraße 44</p> </div>';
+        list.push(["70176","Stuttgart", "Königstraße 44"]);
     } else {
         document.getElementById("standorte").innerHTML = '<div id="no_location"> <p>Keine Geschäftsstelle gefunden</p> </div>';
     }
+    localStorage.setItem("adresse", list);
 }
 
 function submitForm(event) {
@@ -38,13 +44,15 @@ function submitForm(event) {
     var bringenOderVersenden = document.getElementById("bringenOderVersenden").value;
     var abholadresse = document.getElementById("abholadresse").value;
     var plz_abholen = document.getElementById("plz_abholen").value;
-    var naechteGeschaftsstelle; // Variable zum Speichern des ausgewählten Standortes
+    var plz_bringen = document.getElementById("plz_bringen").value;
 
     localStorage.setItem("kleidungsart", kleidungsart);
     localStorage.setItem("krisengebiet", krisengebiet);
     localStorage.setItem("bringenOderVersenden", bringenOderVersenden);
     localStorage.setItem("abholadresse", abholadresse);
+    localStorage.setItem("plz_bringen", plz_bringen);
     localStorage.setItem("plz_abholen", plz_abholen);
+    
 
     window.location.href = 'thankYouPage.html';
 }
---
+++ thankYou.js
@@ -0,0 +1,20 @@
+function onPageLoad() {
+    var kleidungsart = localStorage.getItem("kleidungsart");
+    var krisengebiet = localStorage.getItem("krisengebiet");
+    var bringenOderVersenden = localStorage.getItem("bringenOderVersenden");
+    var abholadresse = localStorage.getItem("abholadresse");
+    var plz_abholen = localStorage.getItem("plz_abholen");
+    var list = localStorage.getItem("adresse", list)
+    console.log(kleidungsart, krisengebiet, bringenOderVersenden, abholadresse, plz_abholen);
+
+    console.log(list);
+
+    if (bringenOderVersenden === "bringen"){
+        document.getElementById("thankYou_form-group").innerHTML = `<h1 style=text-align:center>Vielen Dank...</h1> </br> <h2 class="formular">Bitte bringen Sie Ihre kleidung an die folgende Adresse: <br> ${list}.</h2>`
+    }
+    else if (bringenOderVersenden === "abholen") {
+        document.getElementById("thankYou_form-group").innerHTML = `<h2 class="formular">Der Fahrer kommt in den nächste 2 Stunden an der folgenden Adresse: <br> ${abholadresse}. </h2>`
+    }
+}
+
+    window.addEventListener('load', onPageLoad())
